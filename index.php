414005d75f88c8376140451b699aa8e0b0422fb2
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2024-08-10 21:58:40 +0800

    ehe

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <?php include("includes/header.php"); ?>
 
-        <video autoplay muted loop id="wallpaper">
+        <video id="wallpaper" autoplay muted loop poster="wallpaper.jpeg">
             <source src="wallpaper.mp4" type="video/mp4">
             Your browser does not support HTML5 video.
         </video>
