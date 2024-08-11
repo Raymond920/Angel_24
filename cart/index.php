ed1905de9ae91fe1b6eb198b6b7c01b272324320
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2024-08-11 15:34:34 +0800

    ..

--- cart/index.php
+++ cart/index.php
@@ -1,11 +1,13 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>Angel 24</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../style/mystyle1.css">
     </head>
     <body>
-        <header>
-            <h1>Cart</h1>
-        </header>
-        <?php include('../includes/navigation.php'); ?>
+        <?php include("../includes/header.php"); ?>
+        <?php include("../includes/navigation.php"); ?>
+        <h1>Cart</h1>
     </body>
 </html>
