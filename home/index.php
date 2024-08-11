5251bda26c6d5418bca832b7350953c35499424a
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2024-08-11 15:22:50 +0800

    .

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
     <head>
         <title>Angel 24</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../style/mystyle1.css?v=2.0">
+        <link rel="stylesheet" href="../style/mystyle1.css">
     </head>
     <body>
         <?php include("../includes/header.php"); ?>
