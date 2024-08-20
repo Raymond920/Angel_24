404e70d78f8807584965d312a45ce5d38efa5354
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2024-08-20 19:17:52 +0800

    .

--- cart/index.php
+++ cart/index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,5 +11,9 @@
         <?php include("../includes/header.php"); ?>
         <?php include("../includes/navigation.php"); ?>
         <h1>Cart</h1>
+        <form action="add_to cart.php">
+            <input type="hidden" name="" value="<?php  echo row["prod_id"]?>">
+            <input type="submit" >
+        </form>
     </body>
 </html>
