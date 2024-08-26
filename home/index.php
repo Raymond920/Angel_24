160ce98861ac8d7c683d7a4011a92b87d7d89015
Raymond920 <139645244+Raymond920@users.noreply.github.com>
2024-08-26 20:53:12 +0800

    item drop down

--- home/index.php
+++ home/index.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <title>Angel 24</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="../style/mystyle1.css">
     </head>
     <body>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,8 +1,24 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../style/navigationStyle.css">
+
 <div class="top_nav">
-    <a href="/Angel_24/home">Home</a>
-    <a href="/Angel_24/item">Item</a>
-    <a href="/Angel_24/cart">Cart</a>
-    <a href="/Angel_24/contact">Contact</a>
+    <div class="nav-left">
+        <a href="/Angel_24/home">Home</a>
+        <div class="item-dropdown">
+            <button class="dropbtn">Item
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="item-dropdown-content">
+                <a href="#">Item 1</a>
+                <a href="#">Item 2</a>
+                <a href="#">Item 3</a>
+            </div>
+        </div>
+        <a href="/Angel_24/cart">
+        <i class="fa fa-shopping-cart"></i> Cart</a>
+        <a href="/Angel_24/contact">
+        <i class="fa fa-phone"></i> Contact</a>
+    </div>
     <div class="searchbar">
         <form> 
             <input type="text" placeholder="Search..." name="search">
