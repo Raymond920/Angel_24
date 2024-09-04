f1d18a1f0110c0a12752033f1ac6de61119106e2
rong <chongrq123@gmail.com>
2024-09-04 20:01:02 +0800

    sync

--- includes/logout.php
+++ includes/logout.php
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
 <?php 
     session_start();
     session_destroy();
     header('Location: ../index.php');
     exit();
-=======
-<?php
-    session_start();
-    session_destroy();
-    header('Location: /angel_24/');
->>>>>>> Stashed changes
 ?>
